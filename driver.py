052b05e1e4ca54930fd2fddb4598631dd3097e29
JRSiwiecki <JRSiwiecki@gmail.com>
2022-11-22 21:32:30 -0800

    Add dataset + cycle through dataset for sub img

--- driver.py
+++ driver.py
@@ -1,11 +1,19 @@
 from mosaic import *
+from bing_image_downloader import downloader
+
+# default search words
+search_words = "Red Fruit"
 
 try:
-    img = cv2.imread("apple.png", cv2.IMREAD_COLOR)
+    img = cv2.imread("rainbow.png", cv2.IMREAD_COLOR)
+    
+    # scrape images from bing 💀
+    # search_words = input("What would you like to generate your mosaic out of? ")
+    # downloader.download(search_words, limit=30,  output_dir='dataset', adult_filter_off=False, force_replace=False, timeout=30, verbose=True)
 except:
     print("Can't find that file")
     exit()    
 
-mosaic = MosaicImage(ref_img=img, unit_size=60, enable_debug=True)
+mosaic = MosaicImage(ref_img=img, unit_size=90, enable_debug=True)
 print(mosaic)
 show_image(mosaic.generate_collage())
